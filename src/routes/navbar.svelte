7a1a225ffe569ee6551ccd66ce66b1cdee38d2ad
telaratio <abdoueliasm@gmail.com>
2024-04-06 11:44:51 +0200

    Synchro
    uwu

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,31 +1,31 @@
-<style>
-    navbar
+<svelte:head>
+    <link rel="stylesheet" type="text/css" href="/navbar.css">
+</svelte:head>
 
-    navbar ul {
-        list-style: none;
-    }
-
-    navbar li{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    navbar svg{
-        fill: none;
-        transition: 0.5s;
-    }
-     navbar svg:hover{
-        fill: #000;
-    }
-</style>
 
 <navbar>
-    <img src="" alt="Photo de profil">
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src="/Logo école.png" alt="Photo de profil">
     <ul>
-        <li><svg width="30px" height="30px" viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" id="svg1011" version="1.1">
-            <path id="path8428" d="M41.733 160.134v-59.2H21.999L96 31.865l74 69.067h-19.733v59.201H110.8v-44.4H81.2v44.4z" style="fill-opacity:1;stroke:#000000;stroke-width:12;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:5;stroke-dasharray:none;stroke-opacity:1"/> 
-        </svg>Tableau de bord</li>
-        
+        <li>
+            <svg width="30px" height="30px" viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" id="svg1011" version="1.1">
+            <path id="path8428" d="M41.733 160.134v-59.2H21.999L96 31.865l74 69.067h-19.733v59.201H110.8v-44.4H81.2v44.4z" style="fill-opacity:1;stroke-width:12;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:5;stroke-dasharray:none;stroke-opacity:1"/> 
+            </svg><a href="./tableau-de-bord">Tableau de bord</a>
+        </li>
+        <li>
+            <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 9H21M7 3V5M17 3V5M6 13H8M6 17H8M11 13H13M11 17H13M16 13H18M16 17H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg><a href="">Emploi du temps</a>
+        </li>
+        <li>
+            <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 10.4V20M12 10.4C12 8.15979 12 7.03969 11.564 6.18404C11.1805 5.43139 10.5686 4.81947 9.81596 4.43597C8.96031 4 7.84021 4 5.6 4H4.6C4.03995 4 3.75992 4 3.54601 4.10899C3.35785 4.20487 3.20487 4.35785 3.10899 4.54601C3 4.75992 3 5.03995 3 5.6V16.4C3 16.9601 3 17.2401 3.10899 17.454C3.20487 17.6422 3.35785 17.7951 3.54601 17.891C3.75992 18 4.03995 18 4.6 18H7.54668C8.08687 18 8.35696 18 8.61814 18.0466C8.84995 18.0879 9.0761 18.1563 9.29191 18.2506C9.53504 18.3567 9.75977 18.5065 10.2092 18.8062L12 20M12 10.4C12 8.15979 12 7.03969 12.436 6.18404C12.8195 5.43139 13.4314 4.81947 14.184 4.43597C15.0397 4 16.1598 4 18.4 4H19.4C19.9601 4 20.2401 4 20.454 4.10899C20.6422 4.20487 20.7951 4.35785 20.891 4.54601C21 4.75992 21 5.03995 21 5.6V16.4C21 16.9601 21 17.2401 20.891 17.454C20.7951 17.6422 20.6422 17.7951 20.454 17.891C20.2401 18 19.9601 18 19.4 18H16.4533C15.9131 18 15.643 18 15.3819 18.0466C15.15 18.0879 14.9239 18.1563 14.7081 18.2506C14.465 18.3567 14.2402 18.5065 13.7908 18.8062L12 20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg><a href="">Devoir</a>
+        </li>
+        <li>
+            <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 6H16M8 10H16M8 14H11M6 22H18C19.1046 22 20 21.1046 20 20V4C20 2.89543 19.1046 2 18 2H6C4.89543 2 4 2.89543 4 4V20C4 21.1046 4.89543 22 6 22Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg><a href="">Bulletin</a>
+        </li>
     </ul>
 </navbar>
