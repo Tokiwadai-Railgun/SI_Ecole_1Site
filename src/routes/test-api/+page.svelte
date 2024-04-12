c638e3196ae681940909f733987888ada0c33e18
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2024-04-12 20:37:18 +0200

    Test API

---
+++ src/routes/test-api/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    async function getData() {
+        const response = await fetch("http://localhost:5173/api/users")
+        const data = await response.json();
+        return data;
+    }
+
+    let data: Array<object>;
+    onMount(async() => {
+        data = await getData();
+    })
+</script>
+
+{#if data != null}
+    {#each data as user, i}
+        <h3>User number : {i}</h3>
+        <ul>
+            <li>Name : {user.first_name} {user.last_name}</li>
+            <li>Email : {user.email}</li>
+            <li>Phone : {user.phone_number}</li>
+        </ul>
+        
+    {/each}
+{/if}
