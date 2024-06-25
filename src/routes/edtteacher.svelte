7b784c24b80ad2d91373fede31660be4e32a7746
telaratio <abdoueliasm@gmail.com>
2024-06-25 14:19:49 +0200

    lazygit mdr

---
+++ src/routes/edtteacher.svelte
@@ -0,0 +1,18 @@
+<div class="calendar">
+    <div class="lundi-column">
+        <p class="clalendarhead">Lundi "date"</p>
+        
+    </div>
+    <div class="mardi-column">
+        <p class="clalendarhead">Mardi "date"</p>
+    </div>
+    <div class="mercredi-column">
+        <p class="clalendarhead">Mercredi "date"</p>
+    </div>
+    <div class="jeudi-column">
+        <p class="clalendarhead">Jeudi "date"</p>
+    </div>
+    <div class="vendredi-column">
+        <p class="clalendarhead">Vendredi "date"</p>
+    </div>
+</div>
---
+++ src/routes/emploi-du-temps/+page.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+    <link rel="stylesheet" type="text/css" href="/edt.css">
+</svelte:head>
+<script lang="ts">
+	import Navbar from "../navbar.svelte";
+    import Edtteacher from "../edtteacher.svelte";
+    async function getRole(id: string) {
+        const role = await fetch(`http://localhost:5173/api/user/byId?id=${id}`);
+    }
+    export let data;
+    console.log(data.role);
+    let pageContent;
+
+    if (data.role === "teacher"){
+        pageContent = Edtteacher;
+    }
+</script>
+<div class="global-section">
+    <Navbar />
+    <main>
+        <h1>Emploi du temps</h1>
+        <div class="widget-cont">
+            {#if pageContent}
+                <svelte:component this={pageContent} />
+            {/if}
+        </div>
+    </main>
+</div>
